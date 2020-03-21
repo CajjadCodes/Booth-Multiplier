e83b329603caae8f2836866f8756444b45b82e97
MehdiSaraie <62212279+MehdiSaraie@users.noreply.github.com>
2020-03-21 21:05:18 +0430

    naming

--- reg1.v
+++ reg1.v
@@ -1,12 +1,12 @@
-module reg1(clk,rst,init,ld,in,out);
+module Reg1(clk,rst,init,ld,in,out);
 input clk;
 input rst;
 input init;
 input ld;
 input in;
 output reg out;
-	always@(posedge clk,posedge rst)begin
-		if (rst|init) out<=1'b0;
+	always@(posedge clk or posedge rst)begin
+		if (rst) out<=1'b0;
 		else if (init) out<=1'b0;
 		else if (ld) out<=in;
 	end
