310cd9a9636b74771a316d277d950ff744afd9a6
MehdiSaraie <62212279+MehdiSaraie@users.noreply.github.com>
2020-03-21 20:51:55 +0430

    Update shreg6.v

--- shreg6.v
+++ shreg6.v
@@ -1,6 +1,7 @@
-module shreg6(clk,rst,ld,sh,serin,serout,parin,parout);
+module shreg6(clk,rst,init,ld,sh,serin,serout,parin,parout);
 input clk;
 input rst;
+input init;
 input ld;
 input sh;
 input serin;
@@ -9,6 +10,7 @@ output serout;
 output reg[5:0]parout;
 	always@(posedge clk,posedge rst)begin
 		if (rst) parout<=6'b000000;
+		else if (init) parout<=6'b000000;
 		else if (ld) parout<=parin;
 		else if (sh) parout<={serin,parout[5:1]};
 	end
